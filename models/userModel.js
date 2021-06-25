6fd7e5ff73c0a10a3e454f1a06ac9210c586ad2b
LordGolumpki <66099044+LordGolumpki@users.noreply.github.com>
2021-06-24 21:23:17 -0600

    Added a user model to the db and added bootstrap from validation (validation not currently working because bootstrap 5 is bugged).

---
+++ models/userModel.js
@@ -0,0 +1,18 @@
+// Import external modules
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+// Define user schema
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+});
+
+// Apply passport password functionality to the schema
+UserSchema.plugin(passportLocalMongoose);
+
+// Export schema
+module.exports = mongoose.model("User", UserSchema);
